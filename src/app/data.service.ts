3bc91259b5c66d9a214c205530898e717f25eb6c
kiran <rkirankumar761@gmail.com>
2024-08-17 09:54:57 +0530

    Tested angular pipes

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,7 +10,7 @@ export class DataService {
   constructor() {
 
     this.products = [
-      new Product("s102", "Tata", "Dish", 10, 70),
+      new Product("s103", "Tata", "Dish", 10, 70),
       new Product("s104", "Apple", "iPhone", 20, 999),
       new Product("s105", "Samsung", "Galaxy", 15, 850),
       new Product("s106", "Sony", "PlayStation", 5, 499),
@@ -18,7 +18,7 @@ export class DataService {
       new Product("s108", "LG", "Laptop", 12, 1100),
       new Product("s109", "Nike", "Shoes", 50, 150),
       new Product("s110", "Adidas", "Jacket", 30, 200),
-      new Product("s111", "Apple", "Mustang", 3, 55000),
+      new Product("s111", "Apple", "Mustang", 3, 700),
     ];
     
       
