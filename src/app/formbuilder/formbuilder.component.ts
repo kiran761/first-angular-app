9935879297f702eb123059b26643460cefdfd678
kiran <rkirankumar761@gmail.com>
2024-08-19 12:30:17 +0530

    FormBuilder

--- src/app/formbuilder/formbuilder.component.ts
+++ src/app/formbuilder/formbuilder.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formbuilder',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class FormbuilderComponent {
 
+
+
+  constructor(private formBuilder: FormBuilder) {}
+         productForm = this.formBuilder.group({
+      productid: ['', [Validators.required, Validators.minLength(8)]],
+      brand: [''],
+      description: this.formBuilder.group({
+        age: [0],
+        season: ['']
+      }),
+      qty: [0],
+      price: [0]
+    });
+  
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
