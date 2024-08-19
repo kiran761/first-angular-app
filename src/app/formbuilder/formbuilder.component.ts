9ca02a750fe4c0fb2cc2343a233eb3e3021f6cbd
kiran <rkirankumar761@gmail.com>
2024-08-19 12:54:46 +0530

    Suppliers

--- src/app/formbuilder/formbuilder.component.ts
+++ src/app/formbuilder/formbuilder.component.ts
@@ -8,20 +8,31 @@ import { FormBuilder, FormArray, Validators } from '@angular/forms';
 })
 export class FormbuilderComponent {
 
-
-
   constructor(private formBuilder: FormBuilder) {}
-         productForm = this.formBuilder.group({
-      productid: ['', [Validators.required, Validators.minLength(8)]],
-      brand: [''],
-      description: this.formBuilder.group({
-        age: [0],
-        season: ['']
-      }),
-      qty: [0],
-      price: [0]
-    });
-  
+
+  productForm = this.formBuilder.group({
+    productid: ['', [Validators.required, Validators.minLength(8)]],
+    brand: [''],
+    description: this.formBuilder.group({
+      age: [0],
+      season: ['']
+    }),
+    qty: [0],
+    price: [0],
+    suppliers: this.formBuilder.array([this.formBuilder.control('')]),
+  });
+
+  get suppliers(): FormArray {
+    return this.productForm.get('suppliers') as FormArray;
+  }
+
+  addSupplier() {
+    this.suppliers.push(this.formBuilder.control(''));
+  }
+
+  // removeSupplier(index: number) {
+  //   this.suppliers.removeAt(index);
+  // }
 
   onSubmit() {
     console.log(this.productForm.value);
