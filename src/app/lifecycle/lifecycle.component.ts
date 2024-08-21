2f900a7930372f67f7e5d8c43899f5d81a5fce1c
kiran <rkirankumar761@gmail.com>
2024-08-21 12:41:51 +0530

    just updated

--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrls: ['./lifecycle.component.css']
 })
-export class LifecycleComponent implements OnChanges,OnInit{
-  
+export class LifecycleComponent implements OnChanges,OnInit ,DoCheck,AfterContentInit,AfterContentChecked,AfterViewChecked,AfterViewInit{
+
   constructor(){
     console.log('Constructor implemented.');
   }
+  
+  
 
   @Input() x:string="Kiran"
   ngOnInit(): void {
@@ -19,6 +21,25 @@ export class LifecycleComponent implements OnChanges,OnInit{
   ngOnChanges(changes: SimpleChanges): void {
     console.log('Method OnChanges implemented.');
     console.log(changes);
+  }
+
+  ngDoCheck(){
+    console.log('Method DoCheck implemented.');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('Method AfterContentInit implemented.');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('Method AfterContentChecked implemented.');
+  }
+ 
+  ngAfterViewInit(): void {
+    console.log('Method AfterViewInit implemented.');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Method AfterViewChecked implemented.');
   }
 
 }
